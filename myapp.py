57d2543e336ba66c91a985fa01e868927bb41806
sneh singh <55993455+Sneh713@users.noreply.github.com>
2022-08-17 23:05:30 +0530

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock **closing price** and ***volume*** of Google!
+""")
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+st.write(""" ## closing price """)
+st.line_chart(tickerDf.Close)
+st.write(""" ## volume price """)
+st.line_chart(tickerDf.Volume)
